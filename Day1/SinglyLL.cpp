8b3d0ed058f136739bcc3eb6ad7467ca7adb1538
Mehak Garg <gargmehak1908@gmail.com>
2025-08-01 11:36:15 +0530

    Singly LL Creation using Loop

---
+++ Day1/SinglyLL.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node *next;
+Node(int value){
+data = value;
+next = NULL;
+}
+};
+
+void print(Node *curr){
+while(curr){
+  cout << curr->data << " ";
+  curr = curr->next;
+}
+}
+
+int main(){
+  int arr[5] = {1,2,3,4,5};
+  int size = 5;
+  Node *head = NULL;
+  for(int i = 0; i < size; i++){
+    if(head == NULL){
+      head = new Node(arr[i]);
+    }
+    else{
+    Node *temp = new Node(arr[i]);
+    temp->next = head;
+    head = temp;
+    }
+  }
+cout << "Printing the linked list" << endl;
+print(head);
+}
